633eec674d9eb41b3dab15e6118fdafa4f7658f4
SPbun <835904198@qq.com>
2016-08-02 15:24:49 +0800

    just adjust a little

---
+++ javascript/信安官网.js
@@ -0,0 +1,14 @@
+function w3c()
+{
+	window.open("http://www.w3school.com.cn/");   
+}
+
+function imooc()
+{
+	window.open("http://www.imooc.com");   
+}
+
+function freebuf()
+{
+	window.open("http://www.freebuf.com");   
+}
